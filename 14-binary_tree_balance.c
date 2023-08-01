f00701f14c89d78b60fd64acef3b9c12e27be4d2
eniolagbaike <eniolagbaike@gmail.com>
2023-08-01 02:46:57 -0700

    bug fixed

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,50 +1,46 @@
 #include "binary_trees.h"
 
 /**
- * _binary_tree_height - helper func for binary_tree_height
- * @tree: input tree
- * Return: height of tree
+ * binary_tree_height_b - Measures height of a binary tree for a bal tree
+ * @tree: tree to go through
+ * Return: the height
  */
-size_t _binary_tree_height(const binary_tree_t *tree)
+
+size_t binary_tree_height_b(const binary_tree_t *tree)
 {
-	size_t a, b;
+	size_t l = 0;
+	size_t r = 0;
 
-	if (!tree)
+	if (tree == NULL)
+	{
 		return (0);
-
-	a = _binary_tree_height(tree->left);
-	b = _binary_tree_height(tree->right);
-	return (MAX(a, b) + 1);
+	}
+	else
+	{
+		if (tree)
+		{
+			l = tree->left ? 1 + binary_tree_height_b(tree->left) : 1;
+			r = tree->right ? 1 + binary_tree_height_b(tree->right) : 1;
+		}
+		return ((l > r) ? l : r);
+	}
 }
 
 /**
- * binary_tree_height - measures the height of a binary tree
- * @tree: input tree
- * Return: height of tree
+ * binary_tree_balance - Measures balance factor of a binary tree
+ * @tree: tree to go through
+ * Return: balanced factor
  */
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-	if (!tree)
-		return (0);
-	return (_binary_tree_height(tree) - 1);
-}
 
-/**
- * binary_tree_balance - measures the balance factor of a binary tree
- * @tree: input tree
- * Return: balance factor of descendants
- */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int a, b;
+	int right = 0, left = 0, total = 0;
 
-	if (!tree)
-		return (0);
-	a = (tree->left)
-		? (int)binary_tree_height(tree->left)
-		: -1;
-	b = (tree->right)
-		? (int)binary_tree_height(tree->right)
-		: -1;
-	return (a - b);
+	if (tree)
+	{
+		left = ((int)binary_tree_height_b(tree->left));
+		right = ((int)binary_tree_height_b(tree->right));
+		total = left - right;
+	}
+	return (total);
 }
